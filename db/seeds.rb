38d40e5c6ab81a53583b3143f517d309f68da1a7
Camille Fujisaki <cae224@cornell.edu>
2022-01-11 21:30:57 +0900

    completed index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'detroying all flats'
+Flat.destroy_all
+puts 'destroyed all flats'
+puts 'creating new flats ... '
+10.times do
+  Flat.create(
+    name: Faker::Fantasy::Tolkien.poem,
+    address: Faker::Movies::Hobbit.location,
+    description: "A lovely summer feel for this spacious garden flat. #{rand(2..4)} bedrooms, open plan living area, large kitchen and a beautiful #{["library,", "kitchen", "pool"].sample}",
+    price_per_night: rand(75..250),
+    number_of_guests: rand(1..5)
+  )
+end
+puts 'completed flats'
