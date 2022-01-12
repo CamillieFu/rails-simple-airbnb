3b6c0d0d5816c0c207d10f22460adf2e76cb10d3
Camille Fujisaki <cae224@cornell.edu>
2022-01-12 13:03:45 +0900

    completed show page

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,14 +1,43 @@
 class FlatsController < ApplicationController
+before_action :set_flat, only: [:show, :edit, :update, :destroy]
+
   def index
     @flats = Flat.all
   end
 
   def show
+    @booking = [params[:start], params[:end]]
   end
 
   def new
+    @flat = Flat.new
   end
 
   def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update; end
+
+  def destroy
+    @flat.destroy
+    redirect_to flats_path
+  end
+
+
+  private
+  def flat_params
+    params.require(:flat).permit(:name, :address, :number_of_guests, :description, :price_per_night)
+  end
+
+  def set_flat
+    @flat = Flat.find(params[:id])
   end
 end
